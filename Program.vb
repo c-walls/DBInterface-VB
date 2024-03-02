f005dc87dc546e38b0599b3a3d8dcbc6863e8bb6
C_Walls <calebmarcuswalls@gmail.com>
2024-03-02 10:18:47 -0600

    Connection to DB established

--- Program.vb
+++ Program.vb
@@ -1,7 +1,17 @@
 Imports System
+Imports Oracle.ManagedDataAccess.Client
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Dim connectionString As String = "User Id=ADMIN;Password=StRoNg_password123;Data Source=ovyv9ufieozhf2yr_medium;TNS_ADMIN=C:\\Users\\caleb\\OneDrive\\Documents\\Coding Projects\\Database\\Wallet_OVYV9UFIEOZHF2YR;Pooling=false;"
+        Using connection As New OracleConnection(connectionString)
+            connection.Open()
+            Dim command As New OracleCommand("SELECT * FROM DUAL", connection)
+            Using reader As OracleDataReader = command.ExecuteReader()
+                While reader.Read()
+                    Console.WriteLine(String.Format("{0}", reader(0)))
+                End While
+            End Using
+        End Using
     End Sub
 End Module
