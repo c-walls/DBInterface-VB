1309cb89c1d0b53e72fe82d5d175bb920c8f05b4
C_Walls <calebmarcuswalls@gmail.com>
2024-03-10 22:49:58 -0500

    added dataGridViewer

--- Program.vb
+++ Program.vb
@@ -1,19 +1,21 @@
 Imports System
+Imports System.Data
 Imports System.Drawing' May not need if switching to TableLayoutPanel
 Imports System.Windows.Forms
 Imports Oracle.ManagedDataAccess.Client
 
 ' TO-DO:
 ' 1. Seperate forms into different files
-' 2. Make the dummy call using the button and display it
-' 3. Use a config file to store the connection string values
-' 4. Organize Interface with the TableLayoutPanel manager (Also check for VS code designer extension)
+' 2. Use a config file to store the connection string values
+' 3. Organize Interface with the TableLayoutPanel manager (Also check for VS code designer extension)
 
 Public Class MainForm
     Inherits Form
 
     Private Shared connectionString As String = "User Id=ADMIN;Password=StRoNg_password123;Data Source=ovyv9ufieozhf2yr_medium;TNS_ADMIN=C:\\Users\\caleb\\OneDrive\\Documents\\Coding Projects\\Database\\Wallet_OVYV9UFIEOZHF2YR;Pooling=true;"
     Private connection As OracleConnection
+    Private button As New Button()
+    Private dataViewer As New DataGridView()
 
     Public Sub New()
         ' Initialize the form
@@ -26,15 +28,18 @@ Public Class MainForm
         connection = New OracleConnection(connectionString)
         connection.Open()
 
-        ' Create a button
-        Dim button As New Button()
+        ' Configure button
         button.Text = "Run Query"
         button.Size = New Size(100, 50)
-        button.Location = New Point(screenWidth / 2 - button.Size.Width / 2, screenHeight / 2 - button.Size.Height / 2)
-
-        ' Add button with event handler
+        button.Location = New Point(screenWidth / 2 - button.Size.Width / 2, 100)
         AddHandler button.Click, AddressOf Button_Click
         Me.Controls.Add(button)
+
+        ' Configure DataGridView
+        dataViewer.Size = New Size(screenWidth * (5/6), screenHeight / 2)
+        dataViewer.Location = New Point(screenWidth / 2 - dataViewer.Size.Width / 2, 250)
+        Me.Controls.Add(dataViewer)
+
     End Sub
 
     Private Sub Button_Click(sender As Object, e As EventArgs)
@@ -42,9 +47,9 @@ Public Class MainForm
             connection.Open()
             Dim command As New OracleCommand("SELECT * FROM DUAL", connection)
             Using reader As OracleDataReader = command.ExecuteReader()
-                While reader.Read()
-                    Console.WriteLine(String.Format("{0}", reader(0)))
-                End While
+                Dim dataTable As New DataTable()
+                dataTable.Load(reader)
+                dataViewer.DataSource = dataTable
             End Using
         End Using
     End Sub
