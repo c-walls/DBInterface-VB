7993220a82f08dd4d0b0d96b1eb20a66f56655f7
C_Walls <calebmarcuswalls@gmail.com>
2024-05-01 17:52:42 -0500

    Work Order INSERTS

--- WorkOrderForm.vb
+++ WorkOrderForm.vb
@@ -13,16 +13,18 @@ Public Class WorkOrderPage
     Private workLocationLabel As New Label() With {.Text = "Work Location:"}
     Private workLocationName As New TextBox()
     Private workLocationNameLabel As New Label() With {.Text = "Name:"}
-    Private workLocationAddress As New TextBox() With {.Multiline = True, .Height = workLocationAddress.Height * 2.5}
+    Private workLocationAddress As New TextBox() With {.Multiline = True}
     Private workLocationAddressLabel As New Label() With {.Text = "Address:"}
-    Private workOrderDate As New DateTimePicker() With {.Format = DateTimePickerFormat.Short}
+    Private workOrderDate As New DateTimePicker() With {.Format = DateTimePickerFormat.Custom, .CustomFormat = "MM/dd/yyyy"}
     Private workOrderDateLabel As New Label() With {.Text = "Date:"}
-    Private workOrderNotes As New TextBox() With {.Multiline = True, .Height = workLocationAddress.Height * 2.5}
+    Private workOrderNotes As New TextBox() With {.Multiline = True}
     Private workOrderNotesLabel As New Label() With {.Text = "Notes:"}
     Private manager As New ComboBox() With {.DropDownStyle = ComboBoxStyle.DropDownList}
     Private managerLabel As New Label() With {.Text = "Manager:"}
-    Private dateRequired As New DateTimePicker() With {.Format = DateTimePickerFormat.Short}
+    Private withEvents dateRequired As New DateTimePicker() With {.Format = DateTimePickerFormat.Custom, .CustomFormat = " "}
     Private dateRequiredLabel As New Label() With {.Text = "Date Required:"}
+    Private WithEvents SaveButton As New Button() With {.Text = "Create", .Dock = DockStyle.Top, .Margin = New Padding(0, 120, 0, 0), .Height = 40}
+    Private WithEvents CancelButton As New Button() With {.Text = "Cancel", .Dock = DockStyle.Top, .Margin = New Padding(0, 120, 0, 0), .Height = 40}
 
     Private WithEvents taskOrderDG As New DataGridView() With {.Anchor = AnchorStyles.Left}
     Private taskOrder_DGColumn As New DataGridViewComboBoxColumn() With {.HeaderText = "Task", .Name = "Task"}
@@ -50,17 +52,18 @@ Public Class WorkOrderPage
         tableLayoutPanel.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 14))
         tableLayoutPanel.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 18))
         tableLayoutPanel.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 25))
-        tableLayoutPanel.RowCount = 10
+        tableLayoutPanel.RowCount = 11
         tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 5))
         tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 5))
-        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 5))
-        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 5))
-        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 5))
-        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 5))
-        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 5))
+        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 2))
+        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 8))
+        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 2))
+        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 2))
+        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 8))
         tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 30))
         tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 5))
-        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 30))
+        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 5))
+        tableLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 27))
         Me.Controls.Add(tableLayoutPanel)
 
         ' Configure DataGridView
@@ -101,23 +104,31 @@ Public Class WorkOrderPage
         tableLayoutPanel.Controls.Add(workLocationAddress, 1, 6)
         tableLayoutPanel.Controls.Add(workOrderDateLabel, 3, 2)
         tableLayoutPanel.Controls.Add(workOrderDate, 4, 2)
-        tableLayoutPanel.Controls.Add(workOrderNotesLabel, 3, 4)
-        tableLayoutPanel.Controls.Add(workOrderNotes, 4, 4)
+        tableLayoutPanel.Controls.Add(workOrderNotesLabel, 3, 6)
+        tableLayoutPanel.Controls.Add(workOrderNotes, 4, 6)
         tableLayoutPanel.Controls.Add(taskOrderDG, 0, 7)
         tableLayoutPanel.Controls.Add(managerLabel, 0, 8)
         tableLayoutPanel.Controls.Add(manager, 1, 8)
         tableLayoutPanel.Controls.Add(dateRequiredLabel, 3, 8)
         tableLayoutPanel.Controls.Add(dateRequired, 4, 8)
+        tableLayoutPanel.Controls.Add(SaveButton, 1, 10)
+        tableLayoutPanel.Controls.Add(CancelButton, 3, 10)
 
 
         AddHandler Me.Load, AddressOf WorkOrderPage_Load
+        AddHandler dateRequired.ValueChanged, AddressOf dateRequired_ValueChanged
+        AddHandler SaveButton.Click, AddressOf SaveButton_Click
+        AddHandler CancelButton.Click, AddressOf CancelButton_Click
+        AddHandler taskOrderDG.CellEndEdit, AddressOf taskOrderDG_CellEndEdit
     End Sub
 
     Private Sub WorkOrderPage_Load(sender As Object, e As EventArgs) Handles Me.Load
         PopulateManagersList()
-        'PopulateTasksList()
+        PopulateTasksList()
         proposalNo.Text = selectedProposal
         workOrderNo.Text = "W" & proposalNo.Text.Substring(2, 4) & "-01"
+        workLocationAddress.Size = New Size(200, 35)
+        workOrderNotes.Size = New Size(200, 35)
     End Sub
 
     Private Sub PopulateTasksList()
@@ -141,5 +152,66 @@ Public Class WorkOrderPage
         manager.DataSource = dataTable
         manager.DisplayMember = "Emp_Name"
         manager.ValueMember = "Emp_Name"
+    End Sub
+
+    Private Sub dateRequired_ValueChanged(sender As Object, e As EventArgs) Handles dateRequired.ValueChanged
+        dateRequired.CustomFormat = "MM/dd/yyyy"
+    End Sub
+
+    Private Sub taskOrderDG_CellEndEdit(sender As Object, e As DataGridViewCellEventArgs) Handles taskOrderDG.CellEndEdit
+        Dim value As String = If(taskOrderDG.Rows(e.RowIndex).Cells(e.ColumnIndex).Value IsNot Nothing, taskOrderDG.Rows(e.RowIndex).Cells(e.ColumnIndex).Value.ToString(), 0)
+        Dim selectedTask As String = If(taskOrderDG.Rows(e.RowIndex).Cells(0).Value IsNot Nothing, taskOrderDG.Rows(e.RowIndex).Cells(0).Value.ToString(), String.Empty)
+        Dim maxSQFT As Integer = DBHandler.ExecuteValueQuery("SELECT Total_SQFT FROM TaskRequests WHERE Proposal_No = '" & selectedProposal & "' AND Task_ID IN (SELECT Task_ID FROM Tasks WHERE Task_Names = '" & selectedTask & "')")
+    
+        If (e.ColumnIndex = 1 Or e.ColumnIndex = 2) And Not IsNumeric(value) Then
+            taskOrderDG.Rows(e.RowIndex).Cells(e.ColumnIndex).Value = Nothing
+        End If
+        
+        If e.ColumnIndex = 1 Then
+            If CInt(value) > maxSQFT Then
+                MessageBox.Show($"Square footage exceeds maximum for this task: {maxSQFT}.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                taskOrderDG.Rows(e.RowIndex).Cells(1).Value = Nothing
+            End If
+        End If
+        
+        If e.ColumnIndex = 0 Then
+            taskOrderDG.Rows(e.RowIndex).Cells(3).Value = "--"
+            taskOrderDG.Rows(e.RowIndex).Cells(4).Value = "--"
+        End If
+    End Sub
+
+    Private Sub SaveWorkOrder()
+        Dim orderNo As String = workOrderNo.Text
+        Dim proposalNo As String = proposalNo.Text
+        Dim locationName As String = workLocationName.Text
+        Dim locationAddress As String = workLocationAddress.Text
+        Dim requiredDate As String = dateRequired.Value.ToString("yyyy-MM-dd")
+        Dim orderNotes As String = If(String.IsNullOrEmpty(orderNotesTextBox.Text), String.Empty, orderNotesTextBox.Text)
+        Dim managerID As String = ExecuteValueQuery($"SELECT Emp_ID FROM Employees WHERE Emp_Name = '{manager.Text}'")
+
+        ' INSERT INTO WorkOrders
+        Dim insertWorkOrderStatement As String = $"INSERT INTO WorkOrders (Order_No, Proposal_No, Location_Name, Location_Address, Required_Date, Order_Notes, Manager_ID) VALUES ('{orderNo}', '{proposalNo}', '{locationName}', '{locationAddress}', '{requiredDate}', '{orderNotes}', '{managerID}')"
+        DBHandler.ExecuteStatement(insertWorkOrderStatement)
+
+        ' INSERT INTO TaskOrders
+        For Each row As DataGridViewRow In taskOrderDG.Rows
+            Dim taskName As String = row.Cells(0).Value.ToString()
+            Dim taskID As String = DBHandler.ExecuteValueQuery($"SELECT Task_ID FROM Tasks WHERE Task_Names = '{taskName}'")
+            Dim sqft As Integer = row.Cells(1).Value
+            Dim estHours As Integer = row.Cells(2).Value
+            Dim status As String = "Pending"
+            Dim statement As String = $"INSERT INTO TaskOrders (WorkOrder_No, Task_ID, SquareFeet, EstHours, Status) 
+                                        VALUES ('{workOrderNo.Text}', '{Task_ID}', {sqft}, {estHours}, '{status}')"
+            DBHandler.ExecuteStatement(statement)
+        Next
+    End Sub
+
+    Private Sub SaveButton_Click(sender As Object, e As EventArgs)
+        SaveWorkOrder()
+    End Sub
+
+    Private Sub CancelButton_Click(sender As Object, e As EventArgs)
+        Me.Parent.Controls.Add(New dashboard() With {.Dock = DockStyle.Fill})
+        Me.Parent.Controls.Remove(Me)
     End Sub
 End Class
